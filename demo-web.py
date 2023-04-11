0f678a1042076e61491018cbd57cedff80b3ca77
UmarRamzan <94057389+UmarRamzan@users.noreply.github.com>
2023-04-11 16:47:14 +0500

    Update demo-web.py

--- demo-web.py
+++ demo-web.py
@@ -5,27 +5,17 @@ import streamlit as st
 from scipy.io import wavfile
 from audio_recorder_streamlit import audio_recorder
 
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
-
 @st.cache_resource()
 def load_model(name):
     model = whisper.load_model(name)
     return model
 
-@st.cache_resource()
-def load_model_hugging_face():
-    # load model and processor
-    processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
-    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
-    forced_decoder_ids = processor.get_decoder_prompt_ids(language="urdu", task="translate")
+# placeholder = st.empty()
+# placeholder.caption("Loading Model (May take upto an hour depending on your internet connection)")
 
-placeholder = st.empty()
-placeholder.caption("Loading Model (May take upto an hour depending on your internet connection)")
+# #model = load_model("tiny")
 
-#model = load_model("tiny")
-model = load_model_hugging_face()
-
-placeholder.empty()
+# placeholder.empty()
 
 def transcribe_audio_recording():
 
@@ -36,23 +26,15 @@ def transcribe_audio_recording():
         with st.spinner('Transcribing Audio'):
 
             #transcription = model.transcribe('recording.wav', task="translate", language="urdu", word_timestamps=False)
-            #transcription = replicate.run(
-            #     "openai/whisper:e39e354773466b955265e969568deb7da217804d8e771ea8c9cd0cef6591f8bc",
-            #     input={"audio": open("recording.wav", "rb"), "translate": True, "language": "ur"}
-            # )
+            transcription = replicate.run(
+                "openai/whisper:e39e354773466b955265e969568deb7da217804d8e771ea8c9cd0cef6591f8bc",
+                input={"audio": open("recording.wav", "rb"), "translate": True, "language": "ur"}
+            )
 
-            input_array = wavfile.read('recording.wav')
-            print(input_array)
-            #input_features = processor(input_array, sampling_rate=input_speech["sampling_rate"], return_tensors="pt").input_features
-            # generate token ids
-            #predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
-            # decode token ids to text
-            #transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-            
             st.subheader("Transcription")
 
             #st.markdown(transcription['text'])
-            #st.markdown(transcription['translation'])
+            st.markdown(transcription['translation'])
     
 def transcribe_audio_file():
         
